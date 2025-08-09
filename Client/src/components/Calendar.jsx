a1912d0ba826cbadd3a2530657cc294db9f163a5
Hussain-Bader <7ussain.3ali@gmail.com>
2025-08-09 22:46:40 +0300

    basic calendar

--- Client/src/components/Calendar.jsx
+++ Client/src/components/Calendar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-
+import CalendarTool from 'react-calendar'
+import { useState } from 'react'
+import 'react-calendar/dist/Calendar.css';
 const Calendar = () => {
+  const [date, setDate] = useState(new Date());
+  const onChange = date => {
+    setDate(date)
+  }
   return (
     <div>
-      
+      <CalendarTool onChange={onChange} value={date}/>
+      {console.log(date)}
     </div>
   )
 }
 
 export default Calendar
-
