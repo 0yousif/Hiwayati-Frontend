986a52fd56c0566293f5fd439502f64e85b67a75
Jawad-Hassan-J <Jawad-Hassan-J@gmail.com>
2025-08-11 13:10:58 +0300

    finish cours charts

--- Client/src/components/Chart/CourseChart.jsx
+++ Client/src/components/Chart/CourseChart.jsx
@@ -6,10 +6,40 @@ import { Chart } from 'chart.js/auto'
 const CourseChart = ({userInfo, courseInfo}) => {
   const canvasRef = useRef(null)
  
+   let Courses ={}
+   let courseKey = []
+   let courseValue =[]
 
+   
   useEffect(() => {
 
     console.log('Is connect to chart')
+    console.log("userInfo",userInfo)
+    console.log("courseInfo",courseInfo)
+
+   
+  
+    for(let i=0;i<courseInfo.length;i++){
+      const course = courseInfo[i]
+      Courses[course._id] = course.name
+  
+    }
+   
+
+    for(let i=0;i<userInfo.currentCourses.length;i++){
+      const selectedCourse = userInfo.currentCourses[i]
+      console.log("selectedCourse",selectedCourse)
+      courseKey.push(Courses[selectedCourse.course])
+      courseValue.push(selectedCourse.hours)
+
+
+    }
+
+    console.log("courseKey",courseKey)
+    console.log("courseValue",courseValue)
+
+    console.log("Courses",Courses)
+
   
   }, [])
 
@@ -17,19 +47,14 @@ const CourseChart = ({userInfo, courseInfo}) => {
     const ctx = canvasRef.current.getContext('2d')
 
     const chartInstance = new Chart(ctx, {
-      type: 'pie',
+      type: 'bar',
       data: {
-        labels: [
-          'Education',
-          'Defense',
-          'Healthcare',
-          'Infrastructure',
-          'Other'
-        ],
+        labels: courseKey
+        ,
         datasets: [
           {
-            label: 'Government Spending (Billion $)',
-            data: [120, 200, 150, 90, 50],
+            label: 'Hours',
+            data: courseValue,
             backgroundColor: [
               'rgba(54, 162, 235, 0.6)',
               'rgba(255, 99, 132, 0.6)',
@@ -53,7 +78,7 @@ const CourseChart = ({userInfo, courseInfo}) => {
     }
   }, [])
 
-  return <canvas ref={canvasRef}> </canvas>
+  return (<canvas ref={canvasRef}> </canvas>)
 }
 
 export default CourseChart
--- Client/src/components/Chart/SkillChart.jsx
+++ Client/src/components/Chart/SkillChart.jsx
@@ -6,11 +6,40 @@ import { Chart } from 'chart.js/auto'
 const SkillChart = ({userInfo, courseInfo}) => {
   const canvasRef = useRef(null)
  
+   let Courses ={}
+   let courseKey = []
+   let courseValue =[]
 
+   
   useEffect(() => {
 
     console.log('Is connect to chart')
-    console.log(userInfo,userInfo)
+    console.log("userInfo",userInfo)
+    console.log("courseInfo",courseInfo)
+
+   
+  
+    for(let i=0;i<courseInfo.length;i++){
+      const course = courseInfo[i]
+      Courses[course._id] = course.name
+  
+    }
+   
+
+    for(let i=0;i<userInfo.currentCourses.length;i++){
+      const selectedCourse = userInfo.currentCourses[i]
+      console.log("selectedCourse",selectedCourse)
+      courseKey.push(Courses[selectedCourse.course])
+      courseValue.push(selectedCourse.hours)
+
+
+    }
+
+    console.log("courseKey",courseKey)
+    console.log("courseValue",courseValue)
+
+    console.log("Courses",Courses)
+
   
   }, [])
 
@@ -18,19 +47,14 @@ const SkillChart = ({userInfo, courseInfo}) => {
     const ctx = canvasRef.current.getContext('2d')
 
     const chartInstance = new Chart(ctx, {
-      type: 'line',
+      type: 'bar',
       data: {
-        labels: [
-          'Education',
-          'Defense',
-          'Healthcare',
-          'Infrastructure',
-          'Other'
-        ],
+        labels: courseKey
+        ,
         datasets: [
           {
-            label: 'Government Spending (Billion $)',
-            data: [120, 200, 150, 90, 50],
+            label: 'Hours',
+            data: courseValue,
             backgroundColor: [
               'rgba(54, 162, 235, 0.6)',
               'rgba(255, 99, 132, 0.6)',
